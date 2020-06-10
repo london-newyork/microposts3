b164625602d2a8c0d75306aae17fd1c3fbbe5844
matu <skkxt541@ybb.ne.jp>
2020-06-11 04:20:15 +0900

    ユーザ画面に表示する、お気に入り一覧を取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
 
+  def favorites
+    @user = User.find(params[:id])
+    @favorites = @user.favorites.page(params[:page])
+    counts(@user)
+  end
+
   private
 
   def user_params
