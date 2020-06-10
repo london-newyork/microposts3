06556d6eb05b647dc6a50f75052da22dd47b3158
matu <skkxt541@ybb.ne.jp>
2020-06-11 06:42:01 +0900

    お気に入り機能、もろもろの修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
 
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
 
   validates :content, presence: true, length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :likes, through: :favorites, source: :micropost
 
   def follow(other_user)
